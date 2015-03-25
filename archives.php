50d6505ee14d9f8e4da967165be1c82f790dd17c
Inma Bermejo <inma@inmabermejo.com>
2015-03-25 12:22:36 +0100

    Añadir archivos del nuevo theme responsive

---
+++ archives.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Template Name: Archives
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php the_post(); ?>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+		
+			<div class="archive-search-form"><?php get_search_form(); ?></div>
+			
+			<div class="archive-lists">
+				<div class="archive-list archive-list-topics">
+					<h2>Por temas:</h2>
+					<ul>
+						 <?php wp_list_categories('sort_column=name&optioncount=1'); ?>
+					</ul>
+				</div>
+				<div class="archive-list archive-list-date">
+					<h2>Por fecha:</h2>
+					<ul>
+						<?php wp_get_archives('type=monthly&show_post_count=1'); ?>
+					</ul>
+				</div>
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package basic-20-15
+ */
+?>
+
+	</div><!-- #content -->
+
+			<?php get_sidebar(); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<p>Soy <a href="http://inmabermejo.com" rel="designer">Inma Bermejo</a> y esta web es mi diario personal. Aquí hablo de cosas que me gustan, como el diseño, la lectura o la cocina. También <a href="http://www.chavalina.net/tag/semanal/">escribo sobre mi trabajo</a> como diseñadora y desarrolladora web front-end.</p>
+			<p>Si quieres saber más sobre mi trabajo, puedes visitar mi sitio <a href="http://inmabermejo.com" rel="designer">inmabermejo.com</a> o <a href="/contacto/">escribirme</a>.</div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
